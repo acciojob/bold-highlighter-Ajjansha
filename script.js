fc2b8421bd226732a55f0d2c25d010d2de3eaa04
Ajjansha <141652880+Ajjansha@users.noreply.github.com>
2026-02-06 17:50:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function highlight() {
-    //Write your code here
-const boldWords = document.querySelectorAll('strong'); // Loop through each and change color to green boldWords.forEach(word => { word.style.color = "rgb(0, 128, 0)"; });
+    const boldWords = document.querySelectorAll('strong');
 
+    boldWords.forEach(word => {
+        word.style.color = "rgb(0, 128, 0)";
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
-const boldWords = document.querySelectorAll('strong'); // Loop through each and revert color back to black boldWords.forEach(word => { word.style.color = "rgb(0, 0, 0)"; });
-    
+    const boldWords = document.querySelectorAll('strong');
+
+    boldWords.forEach(word => {
+        word.style.color = "rgb(0, 0, 0)";
+    });
 }
